171542620dc4b8bb5a5c5659d6ce772cdb760ead
Shrira4a <138703217+Shrira4a@users.noreply.github.com>
2024-05-11 09:10:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,10 @@
-"""
-    Calculator
-    ~~~~~~~~~~~~~~
-"""
-import re
-from flask import Flask, jsonify, render_template, request
-
-
+# app.py
+from flask import Flask
 app = Flask(__name__)
 
-
-@app.route('/_calculate')
-def calculate():
-    a = request.args.get('number1', '0')
-    operator = request.args.get('operator', '+')
-    b = request.args.get('number2', '0')
-    # validate the input data
-    m = re.match(r'^\-?\d*[.]?\d*$', a)
-    n = re.match(r'^\-?\d*[.]?\d*$', b)
-
-    if m is None or n is None or operator not in '+-*/':
-        return jsonify(result='Error!')
-
-    if operator == '/':
-        result = eval(a + operator + str(float(b)))
-    else:
-        result = eval(a + operator + b)
-    return jsonify(result=result)
-
-
 @app.route('/')
-def index():
-    return render_template('index.html')
-
+def hello_world():
+    return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
